6e24767fa7f7f35749cff7c8052f463f0d89792c
Junhyeok9904 <cityvet2010@gmail.com>
2023-12-04 01:49:55 +0900

    add admin page

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App2 from './App2';
+import Admin from './Admin';
 import { MetaMaskProvider } from '@metamask/sdk-react';
 import 'bootstrap/dist/css/bootstrap.css'
 import Modal from 'react-modal';
-
+import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 Modal.setAppElement('#root');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,7 +20,13 @@ root.render(
         url: window.location.host,
       }
     }}>
-      <App2 />
+      <BrowserRouter> 
+        <Routes>
+          <Route path="/admin" element={ <Admin /> } /> 
+          <Route path="*" element={ <App2 /> } /> 
+        </Routes>
+      </BrowserRouter>
+      
     </MetaMaskProvider>
   </React.StrictMode>
 );
